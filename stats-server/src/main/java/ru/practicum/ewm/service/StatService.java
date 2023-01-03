8dcfed7fcfd55c56795358ec2a9715c16c44e29f
Oleg <Pisos0399gmail.com@yandex.ru>
2023-01-03 15:43:09 +0300

    03.01.23 refactoring

--- stats-server/src/main/java/ru/practicum/ewm/service/StatService.java
+++ stats-server/src/main/java/ru/practicum/ewm/service/StatService.java
@@ -8,5 +8,6 @@ import java.util.List;
 public interface StatService {
 
     void addHit(EndpointHitDto endpointHitDto);
+
     List<ViewStats> getStats(String start, String end, List<String> uris, Boolean unique);
 }
