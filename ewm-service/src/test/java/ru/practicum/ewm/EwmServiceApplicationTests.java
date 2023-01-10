e7037d39684f764b05c036cfa70f3fee9c6685a4
Oleg <Pisos0399gmail.com@yandex.ru>
2023-01-10 23:26:26 +0300

    10.01.2023

--- ewm-service/src/test/java/ru/practicum/ewm/EwmServiceApplicationTests.java
+++ ewm-service/src/test/java/ru/practicum/ewm/EwmServiceApplicationTests.java
@@ -1,13 +1,1 @@
-package ru.practicum.ewm;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EwmServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
